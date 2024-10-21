658d3c6438c621a55d7e1c94f9a940451425cb29
Navid <torabazary@yahoo.com>
2024-10-21 01:39:06 +0000

    Update IPN

--- example/ipn.php
+++ example/ipn.php
@@ -19,8 +19,8 @@ log::logHeader();
  */
 $ntpIpn = new IPN();
 
-$ntpIpn->activeKey         = 'LXTP-3WDM-WVXL-GC8B-Y5DA';        // activeKey or posSignature
-$ntpIpn->posSignatureSet[] = 'LXTP-3WDM-WVXL-GC8B-Y5DA';        // The active key should be in posSignatureSet as well
+$ntpIpn->activeKey         = 'AAAA-BBBB-CCCC-DDDD-EEEE';        // activeKey or posSignature
+$ntpIpn->posSignatureSet[] = 'AAAA-BBBB-CCCC-DDDD-EEEE';        // The active key should be in posSignatureSet as well
 $ntpIpn->posSignatureSet[] = 'EEEE-AAAA-BBBB-CCCC-DDDD';
 $ntpIpn->posSignatureSet[] = 'FFFF-DDDD-AAAA-BBBB-CCCC'; 
 $ntpIpn->posSignatureSet[] = 'DDDD-FFFF-EEEE-AAAA-BBBB'; 
@@ -28,7 +28,7 @@ $ntpIpn->posSignatureSet[] = 'FFFF-GGGG-HHHH-EEEE-AAAA';
 $ntpIpn->hashMethod        = 'SHA512';
 $ntpIpn->alg               = 'RS512';
 
-$ntpIpn->publicKeyStr = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAy6pUDAFLVul4y499gz1P\ngGSvTSc82U3/ih3e5FDUs/F0Jvfzc4cew8TrBDrw7Y+AYZS37D2i+Xi5nYpzQpu7\nryS4W+qvgAA1SEjiU1Sk2a4+A1HeH+vfZo0gDrIYTh2NSAQnDSDxk5T475ukSSwX\nL9tYwO6CpdAv3BtpMT5YhyS3ipgPEnGIQKXjh8GMgLSmRFbgoCTRWlCvu7XOg94N\nfS8l4it2qrEldU8VEdfPDfFLlxl3lUoLEmCncCjmF1wRVtk4cNu+WtWQ4mBgxpt0\ntX2aJkqp4PV3o5kI4bqHq/MS7HVJ7yxtj/p8kawlVYipGsQj3ypgltQ3bnYV/LRq\n8QIDAQAB\n-----END PUBLIC KEY-----\n";
+$ntpIpn->publicKeyStr = "-----BEGIN PUBLIC KEY-----\ADD-CONTENT-OF-YOUR-PUBLIC-KEY-HERE\n-----END PUBLIC KEY-----\n";
 
 $ipnResponse = $ntpIpn->verifyIPN();
 
